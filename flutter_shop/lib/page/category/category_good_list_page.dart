3bb0c14fae1521e9b229f84b43c252d59c29deb1
tsaievan <tsaievan@icloud.com>
2021-05-30 00:48:02 +0800

    商品列表的初始化

--- flutter_shop/lib/page/category/category_good_list_page.dart
+++ flutter_shop/lib/page/category/category_good_list_page.dart
@@ -1,15 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyrefresh/easy_refresh.dart';
+import 'package:flutter_shop/config/index.dart';
+import 'package:flutter_shop/model/category_good_model.dart';
 
 class CategoryGoodListPage extends StatefulWidget {
-  const CategoryGoodListPage({Key key}) : super(key: key);
+
+  int _firstCategoryId = 0;
+  int _secondCategoryId = 0;
+
+  CategoryGoodListPage(this._firstCategoryId, this._secondCategoryId);
 
   @override
   _CategoryGoodListPageState createState() => _CategoryGoodListPageState();
+
 }
 
 class _CategoryGoodListPageState extends State<CategoryGoodListPage> {
+  var scrollController = ScrollController();
+
+  List<CategoryGoodListModel> _goodList = [];
+  int _pageIndex = 1;
+  int _pageSize = 4;
+  bool _noMoreData = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _getGoodList();
+  }
+
+  _getGoodList() async {
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(KString.GOOD_LIST_TITLE),
+      ),
+      body: Container(),
+    );
   }
 }
